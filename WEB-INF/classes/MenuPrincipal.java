ba273dafc8eb840554b9f472f2e9844bb0623cdd
David <damuocle@teleco.upv.es>
2021-02-14 12:35:05 +0100

    Menu de usuario...

---
+++ WEB-INF/classes/MenuPrincipal.java
@@ -0,0 +1,40 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.sql.*;
+
+public class MenuPrincipal extends HttpServlet{
+    public void doGet (HttpServletRequest req, HttpServletResponse res){
+        Connection con;
+        Statement st;
+        ResultSet rs;
+        String SQL;
+        PrintWriter out;
+        
+        try{
+            //Class.forName("com.mysql.jdbc.Driver");
+            //con=DriverManager.getConnection("jdbc:mysql://127.0.0.1/6enraya","root","");
+            out=res.getWriter();
+            res.setContentType("text/html");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<link rel=\"stylesheet\" href=\"menu.css\">");
+            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>¡BIENVENIDO!</h1>");
+            out.println("<nav >");
+            out.println("<a href=\"#\" class=\"nav-enlace\"> INICIAR PARTIDA </a>");
+            out.println("<a href=\"#\" class=\"nav-enlace\"> MIS PARTIDAS </a>");
+            out.println("<a href=\"#\" class=\"nav-enlace\"> ESTADISTICAS </a>");
+            out.println("<a href=\"#\" class=\"nav-enlace\"> SALIR </a>");
+            out.println("</nav>");
+            out.println("<img src=\"images/tablero.jfif\">");
+            out.println("</body></html>");
+            //con.close();
+            out.close();
+        }catch (Exception e){
+            System.err.println(e);
+        }
+    }//Fin doGet
+}//Fin clase
