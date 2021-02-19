82b7bccc3523f9e54e1f5988efebab32987a27e4
ivvasel <ivvasel@teleco.upv.es>
2021-02-19 17:47:39 +0100

    registro.html y registro.js

---
+++ funciones/registro.js
@@ -0,0 +1,18 @@
+function validar(){             
+    var nick = document.forms["registro"]["nick"].value;
+    var nombre = document.forms["registro"]["nombre"].value;
+    var email = document.forms["registro"]["email"].value;
+    var pdw1 = document.forms["registro"]["pdw1"].value;
+    var pdw2 = document.forms["registro"]["pdw2"].value;
+    var validacion = true;
+    if(nick == "" || nombre =="" || email =="" || pdw1 =="" || pdw2 ==""){
+        alert("Rellene todos los campos");
+        validacion = false;
+    }
+    
+    if (pdw1 !== pdw2 ) {             
+        alert("CONTRASEÑA IGUAL PUTO");             
+        validacion = false;             
+    }   
+    return validacion;          
+}
