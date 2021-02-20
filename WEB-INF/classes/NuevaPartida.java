85a764d2d3c2868ba12bf265e126f4b27664592b
David <damuocle@teleco.upv.es>
2021-02-20 13:53:37 +0100

    Pagina de inicio, menus y opciones de busqueda de contrincantes

--- WEB-INF/classes/NuevaPartida.java
+++ WEB-INF/classes/NuevaPartida.java
@@ -12,23 +12,12 @@ public class NuevaPartida extends HttpServlet{
         PrintWriter out;
         String nombreContrincante;
         
+        
         try{
             nombreContrincante="";
-            Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://127.0.0.1/6enraya","root","");
-            st=con.createStatement();
-            st2=con.createStatement();
-            SQL="SELECT * FROM usuarios WHERE buscaPartida=1";
-            rs=st.executeQuery(SQL);
-            if(!rs.next()){
-               // SQL2="INSERT INTO usuarios (buscaPartida) VALUES (1) WHERE usuarios.Nick='DA'";
-                SQL2="UPDATE usuarios SET buscaPartida=1 WHERE Nick='DAVID'";
-                st2.executeUpdate(SQL2);
-            }else{
-                nombreContrincante=rs.getString(2);
-            }
-            SQL2="UPDATE usuarios SET buscaPartida=1 WHERE Nick='DAVID'";
-            st2.executeUpdate(SQL2);
+            
+           // SQL="SELECT * FROM usuarios WHERE buscaPartida=1";
+            //rs=st.executeQuery(SQL);
             res.setContentType("text/html");
             out=res.getWriter();
             out.println("<html>");
@@ -36,18 +25,14 @@ public class NuevaPartida extends HttpServlet{
             out.println("<link rel=\"stylesheet\" href=\"menu.css\">");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>¿Desea iniciar partida contra "+nombreContrincante+"?</h1>");
+            out.println("<h1>¿Con quién desea iniciar una partida?</h1>");
             out.println("<nav>");
-            out.println("<a href=\"\" class=\"nav-enlace\">SÍ</a>");
-            out.println("<a href=\"\" class=\"nav-enlace\">NO</a>");
+            out.println("<a href=\"nueva/aleatoria\" class=\"nav-enlace\">CONTRINCANTES DISPONIBLES </a>");
+            out.println("<a href=\"amigos\" class=\"nav-enlace\">JUGAR CON UN AMIGO </a>");
             out.println("</nav>");
             out.println("</body>");
-            out.println("</html>");
-            rs.close();
-            st.close();
-            st2.close();
-            con.close();
-            out.close();
+            out.println("</html>"); 
+           
         }catch (Exception e){
             System.err.println(e);
         }
