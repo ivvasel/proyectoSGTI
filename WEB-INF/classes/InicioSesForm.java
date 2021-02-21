495ce45608862819cb00fd3dd7f244e7aca73204
Carlos B <carboro1@teleco.upv.es>
2021-02-21 20:55:12 +0100

    Añadido formulario inicio sesión y servlet que lo procesa. El servlet falta mejorarlo para comprobar primero que usuario existe y luego que contraseña es correcta

---
+++ WEB-INF/classes/InicioSesForm.java
@@ -0,0 +1,25 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class InicioSesForm extends HttpServlet {
+    public void doGet (HttpServletRequest req, HttpServletResponse res) {
+        PrintWriter out;
+
+        try {
+            out = res.getWriter();
+            res.setContentType("text/HTML");
+            out.println("<HTML> <BODY>");
+            out.println("<TITLE> Indica tu usuario y contraseña a continuación </TITLE> <BR>");
+            out.println("<FORM>");
+            out.println("<ACTION=ServletInicio METHOD=POST>");
+            out.println("Usuario: <INPUT TYPE=TEXT NAME=nick> <BR>");
+            out.println("Contraseña: <INPUT TYPE=PASSWORD NAME=pwd> <BR>");
+            out.println("<INPUT TYPE=SUBMIT VALUE=Aceptar>");
+            out.println("</FORM> </BODY> </HTML>");
+            
+            out.close();
+        }
+        catch (Exception e) {System.out.println(e);}
+    }
+}
