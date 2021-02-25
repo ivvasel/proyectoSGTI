456118b280dfa07f81db5e6ee7d3aea206837e02
ivvasel <ivvasel@teleco.upv.es>
2021-02-25 21:12:25 +0100

    Arreglo en ServletRegistro.java

--- WEB-INF/classes/ServletRegistro.java
+++ WEB-INF/classes/ServletRegistro.java
@@ -31,9 +31,9 @@ public class ServletRegistro extends HttpServlet {
             out.println("<tr><td><strong>Usuario</strong></td><td><input name='nick' type='text' /></td></tr>");
             out.println("<tr><td><strong>Nombre</strong></td><td><input name='nombre' type='text' /></td></tr>");
                            
-            out.println("<tr><td><strong>Correo electrï¿½nico</strong></td><td><input name='email' type='email' /></td></tr>");
-            out.println("<tr><td><strong>Contraseï¿½a</strong></td><td><input name='pdw1' type='password' /></td></tr>");
-            out.println("<tr><td><strong>Repita contraseï¿½a</strong></td><td><input name='pdw2' type='password' /></td></tr>");
+            out.println("<tr><td><strong>Correo electrónico</strong></td><td><input name='email' type='email' /></td></tr>");
+            out.println("<tr><td><strong>Contraseña</strong></td><td><input name='pdw1' type='password' /></td></tr>");
+            out.println("<tr><td><strong>Repita contraseña</strong></td><td><input name='pdw2' type='password' /></td></tr>");
                         
             out.println("</table>");
             out.println("<table>");
@@ -65,7 +65,7 @@ public class ServletRegistro extends HttpServlet {
             pwd = req.getParameter("pdw1");
             //color = req.getParameter("Color");
 
-            SQL="SELECT * FROM usuarios WHERE Nick=" + nick;
+            SQL="SELECT * FROM usuarios WHERE Nick='"+nick+"'";
             rs= st.executeQuery(SQL);
             if(rs.next()) {
                 out = res.getWriter();
