46475adb947c7f0a3a25f26b54a66fd1a3fc2ace
David <damuocle@teleco.upv.es>
2021-02-25 12:14:46 +0100

    Formulario Inicio Sesion y check inicio sesion OK

---
+++ WEB-INF/classes/CheckInicioSesion.java
@@ -0,0 +1,55 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+import java.io.*;
+
+public class CheckInicioSesion extends HttpServlet{
+    public void doGet (HttpServletRequest req, HttpServletResponse res){
+        Connection con;
+        Statement st;
+        ResultSet rs;
+        PrintWriter out;
+        String SQL;
+        String nick;
+        String pass;
+        
+        try{
+            nick=req.getParameter("nick");
+            pass=req.getParameter("password");
+            
+            res.setContentType("text/html");
+            out=res.getWriter();
+            
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://127.0.0.1/6enraya","root","");
+            st=con.createStatement();
+            
+            
+            
+            SQL="SELECT * FROM usuarios WHERE Nick='"+nick+"' AND Contraseña='"+pass+"'";
+            rs=st.executeQuery(SQL);
+            
+            if(rs.next()){
+                res.sendRedirect("menu");
+            }else{
+                out.println("<html><body>");
+                out.println("Los datos no son correctos. Por favor, vuelva a intentarlo <br>");
+                out.println("<a href=\"iniciosesionform\">VOLVER A INTENTAR </a>");
+                out.println("</body>");
+                out.println("</html>");
+            }
+            
+            rs.close();
+            st.close();
+            con.close();
+            out.close();
+        }catch (Exception e){
+            System.err.println(e);
+        }
+    }
+}
+
+                
+                
+               
+            
