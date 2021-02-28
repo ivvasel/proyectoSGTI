7b0bbb1bddf2e0be98502b23bd5dceb2042348bd
David <damuocle@teleco.upv.es>
2021-02-28 12:22:36 +0100

    Nuevos servlets y posibles avances

--- WEB-INF/classes/Tabla.java
+++ WEB-INF/classes/Tabla.java
@@ -4,67 +4,88 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class Tabla extends HttpServlet{
-    public void doGet (HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
-        res.setContentType("text/html");
-        PrintWriter out=res.getWriter();
-        String tablero[][]=new String [6][6];
-        String BO1=req.getParameter("BO1");
-        String BO2=req.getParameter("BO2");
-        String BO3=req.getParameter("BO3");
-        String BO4=req.getParameter("BO4");
-        String BO5=req.getParameter("BO5");
-        String BO6=req.getParameter("BO6");
+    public void doGet (HttpServletRequest req, HttpServletResponse res){
+        Connection con;
+        Statement st;
+        ResultSet rs;
+        String SQL;
+        PrintWriter out;
+        HttpSession sesion;
+        String tablero[][];
+        String boton1,boton2,boton3,boton4,boton5,boton6;
+        boolean columna_libre;
+        try{
+            boton1=req.getParameter("BO1");
+            boton2=req.getParameter("BO2");
+            boton3=req.getParameter("BO3");
+            boton4=req.getParameter("BO4");
+            boton5=req.getParameter("BO5");
+            boton6=req.getParameter("BO6");
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://127.0.0.1/6enraya","root","");
+            sesion=(HttpSession) req.getSession();
+            tablero=(String[][]) sesion.getAttribute("tablero");
 
-        for (int i=0;i<tablero.length;i++){ //Inicio matriz de las fichas
-            for (int j=0;j<tablero.length;j++){
-                tablero[i][j]="";
+            if(boton1!=null){
+                //metodo
+            }else if(boton2!=null){
+                //metodo
+            }else if(boton3!=null){
+                //metodo
+            }else if(boton4!=null){
+                //metodo
+            }else if(boton5!=null){
+                //metodo
+            }else{
+                //metodo
             }
-        }
 
-        /* for (int i=0;i<3;i++){
-        for (int j=0;j<tablero.length;j++){
-        tablero[i][j]="<div class=\"ficharoja\"></div>";
-        }
-        }*/
+            
+            res.setContentType("text/html");
+            out=res.getWriter();
 
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<link rel="+"\"stylesheet\"" +"href="+"\"index.css\""+">");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<form action="+"\"tabla\"" +">");
-        out.println("<table width=" +"\"auto\"" +"height=" +"\"auto\"" +"; border=" +"\"1\"" +"cellspacing=" +"\"2\"" 
-            +"cellpadding=" +"\"2\"" +">");
-        out.println("<tr align=" +"\"center\"" +">");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<link rel="+"\"stylesheet\"" +"href="+"\"index.css\""+">");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<form action="+"\"tabla\"" +">");
+            out.println("<table width=" +"\"auto\"" +"height=" +"\"auto\"" +"; border=" +"\"1\"" +"cellspacing=" +"\"2\"" 
+                +"cellpadding=" +"\"2\"" +">");
+            out.println("<tr align=" +"\"center\"" +">");
 
-        /* for(int i=1; i<7; i++){
-        out.println("<td><button id=" +"\"col"+i+"\"" + ">Ficha aqui"
-        +"</button></td>");
-        }*/
+            /* for(int i=1; i<7; i++){
+            out.println("<td><button id=" +"\"col"+i+"\"" + ">Ficha aqui"
+            +"</button></td>");
+            }*/
 
-        for(int i=1;i<7;i++){
-            out.println("<td class="+"\"filabotones\""+">"+"<input type=" +"\"submit\"" +"name="+"\"BO"+i+"\"" +"value="+"\"BO"+i+"\""+"class="+
-                "\"invisible\""+">");
-        }
-        out.println("</tr>");
-        out.println("</table>"); //FIN Primera fila de botones
-        out.println("</form>");
-
-        //Tablero para fichas
-        out.println("<table width=" +"\"530px\"" +"height=" +"\"530px\"" +"; border=" +"\"1\"" +"cellspacing=" +"\"2\"" 
-            +"cellpadding=" +"\"2\"" +">");   
-
-        for (int i=1;i<7;i++){
-            out.println("<tr class=\"filatablero\" align=" +"\"center\"" +">");
-            for (int j=1;j<7;j++){                
-                out.println("<td class=\"casillas\" id="+"\"cas"+i+j+"\">"+tablero[i-1][j-1]+"</td>");
+            for(int i=1;i<7;i++){
+                out.println("<td class="+"\"filabotones\""+">"+"<input type=" +"\"submit\"" +"name="+"\"BO"+i+"\"" +"value="+"\"BO"+i+"\""+"class="+
+                    "\"invisible\""+">");
             }
             out.println("</tr>");
+            out.println("</table>"); //FIN Primera fila de botones
+            out.println("</form>");
+
+            //Tablero para fichas
+            out.println("<table width=" +"\"530px\"" +"height=" +"\"530px\"" +"; border=" +"\"1\"" +"cellspacing=" +"\"2\"" 
+                +"cellpadding=" +"\"2\"" +">");   
+
+            for (int i=1;i<7;i++){
+                out.println("<tr class=\"filatablero\" align=" +"\"center\"" +">");
+                for (int j=1;j<7;j++){                
+                    out.println("<td class=\"casillas\" id="+"\"cas"+i+j+"\">"+tablero[i-1][j-1]+"</td>");
+                }
+                out.println("</tr>");
+            }
+            out.println("</table>");
+            out.println("</body>");
+            out.println("</html>");
+        } catch (Exception e){
+            System.err.println(e);
+
         }
-        out.println("</table>");
-        out.println("</body>");
-        out.println("</html>");
+
     }
-    
-    
+
 }
